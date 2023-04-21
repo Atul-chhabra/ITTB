ff9445c610b5367bed3b47c22a5844e442aa2880
Atul-chhabra <59210055+Atul-chhabra@users.noreply.github.com>
2023-04-21 08:38:04 +0530

    7 files modified

--- src/page-banner/page-banner.component.ts
+++ src/page-banner/page-banner.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-banner',
   templateUrl: './page-banner.component.html',
-  styleUrls: ['./page-banner.component.css']
+  styleUrls: ['./page-banner.component.css', '../css/main.css']
 })
 export class PageBannerComponent implements OnInit {
 
