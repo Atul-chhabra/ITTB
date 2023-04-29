e90d47deb469d1c1886c92050626834bb6814e26
Atul-chhabra <59210055+Atul-chhabra@users.noreply.github.com>
2023-04-29 21:05:51 +0530

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from '../header/header.component';
 import { PageBannerComponent} from '../page-banner/page-banner.component';
 import {RegistrationComponent} from '../registration/registration.component';
+import {FooterComponent} from '../footer/footer.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent,HeaderComponent, PageBannerComponent, RegistrationComponent],
+  declarations: [AppComponent,HeaderComponent, PageBannerComponent, RegistrationComponent, FooterComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
