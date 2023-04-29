d924413baa880eaab2728a089b4275c871b763a5
Atul-chhabra <59210055+Atul-chhabra@users.noreply.github.com>
2023-04-30 02:19:26 +0530

    5 files modified

--- src/header/header.component.ts
+++ src/header/header.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss', '../css/main.css']
 })
 export class HeaderComponent implements OnInit {
+  phoneNumber: String = '+91 8766792884';
+  email: String = 'ittb50jobs@ITTB-50.com';
 
   constructor() { }
 
